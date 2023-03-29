f4557ca33a24fb31677e50709142e1b208f3678f
JJ-practice <fixymefixyou@gmail.com>
2023-03-29 16:21:59 +0800

    build

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,16 @@
-import { ThemeProvider } from 'next-themes'
+'use client'
+
+import { ServerThemeProvider, ThemeProvider } from 'next-themes'
 import Footer from './Footer'
 import Navbar from './components/Navbar'
 import ProfilePic from './components/ProfilePic'
 import HeroSection from './components/HeroSection'
 import type { Metadata } from 'next'
 
-export const metadata = {
-  title: 'Josh&apos;s dev blog',
-  description: 'Created by Josh Hong. All rights reserved',
-}
+// export const metadata = {
+//   title: 'Josh&apos;s dev blog',
+//   description: 'Created by Josh Hong. All rights reserved',
+// }
 
 /// fonts to use
 import { Karla } from '@next/font/google'
