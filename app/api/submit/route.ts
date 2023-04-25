78d34dad846669f1f80f26739106f9eab4d14013
JJ-practice <fixymefixyou@gmail.com>
2023-04-25 15:55:48 +0800

    message origin

--- app/api/submit/route.ts
+++ app/api/submit/route.ts
@@ -1,66 +1,48 @@
-import { google, Auth as googleAuth } from 'googleapis'
-import type { NextApiRequest, NextApiResponse } from 'next'
+import { google } from 'googleapis'
+
 type SheetForm = {
   visitor: string
   email: string
   message: string
 }
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  if (req.method === 'POST') {
-    // Parse the JSON string from the environment variable
-    const credentials = JSON.parse(
-      process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON ?? '{}'
-    )
+export async function POST(req: Request) {
+  // auth
+  const auth = await google.auth.getClient({
+    scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+  })
+  const sheets = google.sheets({ version: 'v4', auth })
 
-    // auth
-    const auth = new googleAuth.JWT(
-      credentials.client_email,
-      undefined,
-      credentials.private_key,
-      ['https://www.googleapis.com/auth/spreadsheets'],
-      undefined
-    )
-    const sheets = google.sheets({ version: 'v4', auth })
+  // query
+  const range = 'message!A:D'
+  const body = await req.json()
+  const time = new Intl.DateTimeFormat('zh-TW', {
+    year: 'numeric',
+    month: 'numeric',
+    day: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+  }).format(new Date())
 
-    // query
-    const range = 'message!A:D'
-    const body = req.body as SheetForm
-    const time = new Intl.DateTimeFormat('zh-TW', {
-      year: 'numeric',
-      month: 'numeric',
-      day: 'numeric',
-      hour: '2-digit',
-      minute: '2-digit',
-    }).format(new Date())
+  try {
+    const response = await sheets.spreadsheets.values.append({
+      spreadsheetId: process.env.SHEET_ID,
+      range,
+      valueInputOption: 'USER_ENTERED',
+      requestBody: {
+        values: [[time, body.visitor, body.email, body.message]],
+      },
+    })
 
-    try {
-      const response = await sheets.spreadsheets.values.append({
-        spreadsheetId: process.env.SHEET_ID,
-        range,
-        valueInputOption: 'USER_ENTERED',
-        requestBody: {
-          values: [[time, body.visitor, body.email, body.message]],
-        },
-      })
+    console.log('Response data:', response.data)
 
-      console.log('Response data:', response.data)
-
-      return new Response(JSON.stringify({ status: 'success' }), {
-        headers: { 'Content-Type': 'application/json' },
-      })
-    } catch (error) {
-      console.error('Error appending data to Google Sheet:', error)
-      return new Response(JSON.stringify({ status: 'error' }), {
-        headers: { 'Content-Type': 'application/json' },
-        status: 500,
-      })
-    }
-  } else {
-    // Handle other HTTP methods
-    res.setHeader('Allow', ['POST'])
-    res.status(405).end(`Method ${req.method} Not Allowed`)
+    return new Response(JSON.stringify({ status: 'success' }), {
+      headers: { 'Content-Type': 'application/json' },
+    })
+  } catch (error) {
+    console.error('Error appending data to Google Sheet:', error)
+    return new Response(JSON.stringify({ status: 'error' }), {
+      headers: { 'Content-Type': 'application/json' },
+      status: 500,
+    })
   }
 }
