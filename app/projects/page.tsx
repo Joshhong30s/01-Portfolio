cddaf81c4a8016fad184ca3b3d4f621536c23dca
JJ-practice <fixymefixyou@gmail.com>
2023-04-28 16:40:54 +0800

    0428

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -2,12 +2,23 @@
 
 import Image from 'next/legacy/image'
 import Link from 'next/link'
+import { IoLogoReact } from 'react-icons/io5'
+import {
+  SiGooglesheets,
+  SiMongodb,
+  SiTailwindcss,
+  SiTypescript,
+  SiExpress,
+} from 'react-icons/si'
+
+import { SiNextdotjs } from 'react-icons/si'
 
 interface Project {
   id: string
   title: string
-  intro: string
+  stack: JSX.Element[]
   description: string
+  features: string[]
   imageUrl: string
   liveUrl: string
   repoUrl: string
@@ -16,38 +27,91 @@ interface Project {
 const projects: Project[] = [
   {
     id: '1',
-    title: 'Recipe Sharing App',
-    intro: 'TBD',
-    description: 'MongoDB, Express.JS, React, NodeJS, Backend',
-    imageUrl: '/02blog.jpg',
-    liveUrl: 'https://jjreactblog.netlify.app',
-    repoUrl: 'https://github.com/Joshhong30s/02blog-react',
+    title: 'Baby Photo Album Guestbook',
+    stack: [
+      <SiNextdotjs key='nextjs' size={30} />,
+      <SiTailwindcss key='tailwindcss' size={30} />,
+      <SiTypescript key='typescript' size={30} />,
+      <SiMongodb key='typescript' size={30} />,
+      <SiExpress key='typescript' size={30} />,
+    ],
+    description:
+      'A website I made for my newborn son, updated daily with photos and growth data',
+    features: [
+      'Photo & Video Album (Mongodb)',
+      'User photo uploads (Imgur API)',
+      'Favorites access',
+      'Growth Dashboard (React-echart)',
+      'Growth Data (React-table)',
+      'Dynamic Guestbook (Google API)',
+      'Login & Logout (Mongodb)',
+      'RWD',
+    ],
+    imageUrl: '/06baby.jpg',
+    liveUrl: 'https://06baby.vercel.app/',
+    repoUrl: '',
   },
   {
     id: '2',
-    title: 'Todo Kanban App',
-    intro:
-      'This is a simple drag-and-drop To-do List, I use it for remembering everything I need to do after my baby born. This app is fully responsive and uses local storage, so I can easily track and get all of the things done easily.',
-    description: 'Next.js, React-beautiful-dnd, Chakra UI',
+    title: 'Drag and Drop Todo List Kanban',
+    stack: [
+      <IoLogoReact key='nextjs' size={30} />,
+      <SiTailwindcss key='tailwindcss' size={30} />,
+      <SiTypescript key='typescript' size={30} />,
+    ],
+    description:
+      'A simple Todo List with Kanban-like drag and drop functionality',
+    features: [
+      'Todo',
+      'Drag and Drop List (React DnD)',
+      'Local Storage',
+      'RWD',
+    ],
     imageUrl: '/08todo.jpg',
     liveUrl: 'https://08-simple-todo-kanban.vercel.app',
     repoUrl: 'https://github.com/Joshhong30s/08SimpleTodoKanban',
   },
   {
     id: '3',
-    title: 'React Dashboard',
-    intro: 'TBD',
-    description: 'TBD',
-    imageUrl: '',
+    title: 'Family Health Dashboard',
+    stack: [
+      <SiNextdotjs key='nextjs' size={30} />,
+      <SiTailwindcss key='tailwindcss' size={30} />,
+      <SiTypescript key='typescript' size={30} />,
+      <IoLogoReact key='nextjs' size={30} />,
+    ],
+    description:
+      'An app for the whole family to record health status and provide a monitoring dashboard',
+    features: [
+      'Dashboard (React-rechart)',
+      'Table (react-table)',
+      'User management',
+      'Weather API integration',
+      'RWD',
+    ],
+    imageUrl: '/02blog.jpg',
     liveUrl: 'TBD',
     repoUrl: 'TBD',
   },
   {
     id: '4',
-    title: 'Personal Dev Blog',
-    intro:
-      'This is where you are now. This website is made by NextJS, with simple blog functionality, light/dark mode switch, English/Chinese switch, providing good user experience to users interested in my dev jounrney and projects.',
-    description: 'Next.JS, Tailwind CSS, Typescript, Markdown Blog',
+    title: 'Personal Tech Blog',
+    stack: [
+      <SiNextdotjs key='nextjs' size={30} />,
+      <SiTailwindcss key='tailwindcss' size={30} />,
+      <SiTypescript key='typescript' size={30} />,
+      <SiGooglesheets key='typescript' size={30} />,
+    ],
+    description:
+      'A personal website to showcase my projects and serve as a tech blog',
+    features: [
+      'Markdown blog',
+      'Contact Form (Google Sheet API)',
+      'Project showcase',
+      'Dark mode toggle',
+      'English/Chinese language switch',
+      'RWD',
+    ],
     imageUrl: '/01portfolio.jpg',
     liveUrl: 'https://joshhong.vercel.app',
     repoUrl: 'https://github.com/Joshhong30s/01-Portfolio',
@@ -64,7 +128,7 @@ const Projects = () => {
         {projects.map((project) => (
           <div
             key={project.id}
-            className='group rounded-lg shadow-sm p-6 bg-gray-50 dark:bg-slate-700 h-auto min-h-[400px] transition-all duration-300 transform hover:scale-105 hover:shadow-lg dark:hover:bg-gray-700'
+            className='group rounded-lg shadow-sm p-6 bg-gray-50 dark:bg-slate-700 h-auto min-h-[400px] transition-all duration-300 transform hover:scale-105 hover:shadow-lg dark:hover:bg-gray-700 flex flex-col'
           >
             <div className='relative overflow-hidden border-teal-600 rounded-md mb-6 w-full h-64'>
               <Link
@@ -78,16 +142,28 @@ const Projects = () => {
                   alt=''
                   layout='fill'
                 />
-                <div className='absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-slate-900 bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-95 hover:text-white text-xl p-6'>
-                  {project.intro}
-                </div>
               </Link>
             </div>
-            <h3 className='text-3xl font-semibold mb-4'>{project.title}</h3>
-            <p className='text-xl font-medium text-slate-800 dark:text-gray-100 mb-6 h-36'>
+            <h3 className='text-3xl font-semibold text-slate-900 dark:text-gray-100 mb-6 px-4 py-1 text-center rounded-md'>
+              {project.title}
+            </h3>
+            <p className='text-xl font-medium text-slate-700 dark:text-gray-300 mb-6'>
               {project.description}
             </p>
-            <div className='flex justify-between'>
+
+            {/* Render features as an unordered list */}
+            <ul className='list-none pl-0 text-xl font-medium text-slate-800 dark:text-gray-100 mb-6 flex-grow space-y-2'>
+              {project.features.map((feature, index) => (
+                <li key={index} className='mb-2 pl-4'>
+                  <span className='inline-block w-2 h-2 bg-slate-800 dark:bg-gray-100 mr-3 rounded-full'></span>
+                  {feature}
+                </li>
+              ))}
+            </ul>
+            <p className='text-2xl flex gap-4 font-medium text-slate-800 dark:text-gray-200 h-6 px-4 py-1 rounded-md mb-12 justify-start'>
+              {project.stack}
+            </p>
+            <div className='flex justify-between mt-auto'>
               <Link
                 className='text-blue-800 dark:text-blue-400 hover:text-blue-600 dark:hover:text-blue-300 font-semibold'
                 href={project.liveUrl}
--- app/zh/projects/page.tsx
+++ app/zh/projects/page.tsx
@@ -1,13 +1,24 @@
-// Projects.tsx
 'use client'
 
 import Image from 'next/legacy/image'
 import Link from 'next/link'
+import { IoLogoReact } from 'react-icons/io5'
+import {
+  SiGooglesheets,
+  SiMongodb,
+  SiTailwindcss,
+  SiTypescript,
+  SiExpress,
+} from 'react-icons/si'
+
+import { SiNextdotjs } from 'react-icons/si'
 
 interface Project {
   id: string
   title: string
+  stack: JSX.Element[]
   description: string
+  features: string[]
   imageUrl: string
   liveUrl: string
   repoUrl: string
@@ -16,32 +27,82 @@ interface Project {
 const projects: Project[] = [
   {
     id: '1',
-    title: '食譜分享網站',
-    description: 'MongoDB, Express.JS, React, NodeJS, Backend',
-    imageUrl: '/02blog.jpg',
-    liveUrl: 'https://jjreactblog.netlify.app',
-    repoUrl: 'https://github.com/Joshhong30s/02blog-react',
+    title: '寶寶相簿留言版',
+    stack: [
+      <SiNextdotjs key='nextjs' size={30} />,
+      <SiTailwindcss key='tailwindcss' size={30} />,
+      <SiTypescript key='typescript' size={30} />,
+      <SiMongodb key='typescript' size={30} />,
+      <SiExpress key='typescript' size={30} />,
+    ],
+    description: '為剛出生的兒子做的網站，每天更新相簿與成長數據',
+    features: [
+      '相片&影片相簿(Mongodb)',
+      '使用者上傳相片(Imugr api)',
+      '我的最愛存取',
+      '成長儀錶板(React-echart)',
+      '成長數據(React-table)',
+      '動態留言板(Googleapi)',
+      '登入登出(Mongodb)',
+      'RWD',
+    ],
+    imageUrl: '/06baby.jpg',
+    liveUrl: 'https://06baby.vercel.app/',
+    repoUrl: '',
   },
   {
     id: '2',
     title: '拖拉待辦清單看板',
-    description: 'Next.js, React-beautiful-dnd, Chakra UI',
+    stack: [
+      <IoLogoReact key='nextjs' size={30} />,
+      <SiTailwindcss key='tailwindcss' size={30} />,
+      <SiTypescript key='typescript' size={30} />,
+    ],
+    description: '簡易的Todo List加上類似看板的拖拉功能',
+    features: ['Todo', '看板拖拉清單(React DnD)', 'Local Storage', 'RWD'],
     imageUrl: '/08todo.jpg',
     liveUrl: 'https://08-simple-todo-kanban.vercel.app',
     repoUrl: 'https://github.com/Joshhong30s/08SimpleTodoKanban',
   },
   {
     id: '3',
-    title: 'React Dashboard',
-    description: 'TBD',
-    imageUrl: '',
+    title: '全家人的健康儀錶板',
+    stack: [
+      <SiNextdotjs key='nextjs' size={30} />,
+      <SiTailwindcss key='tailwindcss' size={30} />,
+      <SiTypescript key='typescript' size={30} />,
+      <IoLogoReact key='nextjs' size={30} />,
+    ],
+    description: '給全家人紀錄健康狀況，並提供監控的儀表板App',
+    features: [
+      'Dashboard(React-rechart)',
+      'Table(react-table)',
+      '使用者管理',
+      '天氣API串接',
+      'RWD',
+    ],
+    imageUrl: '/02blog.jpg',
     liveUrl: 'TBD',
     repoUrl: 'TBD',
   },
   {
     id: '4',
-    title: '個人網站暨技術部落格',
-    description: 'Next.JS, Tailwind CSS, Typescript, Markdown Blog',
+    title: '個人技術部落格',
+    stack: [
+      <SiNextdotjs key='nextjs' size={30} />,
+      <SiTailwindcss key='tailwindcss' size={30} />,
+      <SiTypescript key='typescript' size={30} />,
+      <SiGooglesheets key='typescript' size={30} />,
+    ],
+    description: '作為個人的網站展示Project，同時作為技術部落格使用',
+    features: [
+      'Markdown的部落格',
+      '聯絡留言板(GoogleSheet Api)',
+      '專案展示',
+      '深色模式切換',
+      '中英語言切換',
+      'RWD',
+    ],
     imageUrl: '/01portfolio.jpg',
     liveUrl: 'https://joshhong.vercel.app',
     repoUrl: 'https://github.com/Joshhong30s/01-Portfolio',
@@ -58,7 +119,7 @@ const Projects = () => {
         {projects.map((project) => (
           <div
             key={project.id}
-            className='group rounded-lg shadow-sm p-6 bg-gray-50 dark:bg-slate-700 h-auto min-h-[400px] transition-all duration-300 transform hover:scale-105 hover:shadow-lg dark:hover:bg-gray-700'
+            className='group rounded-lg shadow-sm p-6 bg-gray-50 dark:bg-slate-700 h-auto min-h-[400px] transition-all duration-300 transform hover:scale-105 hover:shadow-lg dark:hover:bg-gray-700 flex flex-col'
           >
             <div className='relative overflow-hidden border-teal-600 rounded-md mb-6 w-full h-64'>
               <Link
@@ -74,11 +135,26 @@ const Projects = () => {
                 />
               </Link>
             </div>
-            <h3 className='text-3xl font-semibold mb-4'>{project.title}</h3>
-            <p className='text-xl font-medium text-slate-800 dark:text-gray-100 mb-6 h-36'>
+            <h3 className='text-3xl font-semibold text-slate-900 dark:text-gray-100 mb-6 px-4 py-1 text-center rounded-md'>
+              {project.title}
+            </h3>
+            <p className='text-xl font-medium text-slate-700 dark:text-gray-300 mb-6'>
               {project.description}
             </p>
-            <div className='flex justify-between'>
+
+            {/* Render features as an unordered list */}
+            <ul className='list-none pl-0 text-xl font-medium text-slate-800 dark:text-gray-100 mb-6 flex-grow space-y-2'>
+              {project.features.map((feature, index) => (
+                <li key={index} className='mb-2 pl-4'>
+                  <span className='inline-block w-2 h-2 bg-slate-800 dark:bg-gray-100 mr-3 rounded-full'></span>
+                  {feature}
+                </li>
+              ))}
+            </ul>
+            <p className='text-2xl flex gap-4 font-medium text-slate-800 dark:text-gray-200 h-6 px-4 py-1 rounded-md mb-12 justify-start'>
+              {project.stack}
+            </p>
+            <div className='flex justify-between mt-auto'>
               <Link
                 className='text-blue-800 dark:text-blue-400 hover:text-blue-600 dark:hover:text-blue-300 font-semibold'
                 href={project.liveUrl}
